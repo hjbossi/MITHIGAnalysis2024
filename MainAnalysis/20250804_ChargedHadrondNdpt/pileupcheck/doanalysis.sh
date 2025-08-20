d89bd123cf1bec7891d584c2db640e508e017c4f
Abraham Holtermann <85201358+AHoltermann@users.noreply.github.com>
2025-08-20 18:23:25 -0400

    pu study comparison (#212)

---
+++ MainAnalysis/20250804_ChargedHadrondNdpt/pileupcheck/doanalysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MODE="output_hipileup"
+./RunParallelReadParam.sh \
+    --Output ${MODE} \
+    --Input ../NeNe_InputFileList.txt \
+    --CollisionSystem true \
+    --ApplyEventSelection true \
+    --UseEventWeight true \
+    --UseSpeciesWeight true \
+    --UseTrackWeight true \
+    --TrackWeightSelection 2 \
+    --TriggerChoice 1 \
+    --EventSelectionOption 2 \
+    --SpeciesCorrectionOption 2 \
+    --ScaleFactor 1
+    
