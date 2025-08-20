bd6c86dd39bf14f84e45644a0ef61756faf86948
Hannah <hannah.bossi@cern.ch>
2025-08-20 03:24:05 +0200

    Add OO min bias

--- Theory/20250819_pQCDEloss/makeGraphFromJSON.C
+++ Theory/20250819_pQCDEloss/makeGraphFromJSON.C
@@ -9,13 +9,53 @@
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
-void makeGraphFromJSON() {
-    std::ifstream in("neneOutput.json");
+TGraphAsymmErrors*  getGraphFromJSON(std::string inputFile, std::string datasetName);
+
+
+void makeGraphFromJSON(){
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  
+  TGraphAsymmErrors* g = (TGraphAsymmErrors*) getGraphFromJSON("neneOutput.json", "NeNe_000_100_Pi0"); 
+  TGraphAsymmErrors* g2 = (TGraphAsymmErrors*) getGraphFromJSON("oxygenOutput.json", "OO_000_100_Pi0");
+  
+  // plot just to check if it's correct
+  TCanvas *c = new TCanvas("c","c",800,600);
+  g->Draw("A3");   // "3" = draw filled band
+  g->GetHistogram()->GetXaxis()->SetTitle("#it{p}_{T}");
+  g->GetHistogram()->GetYaxis()->SetTitle("#it{R}_{AA}");
+  g->GetHistogram()->GetYaxis()->SetRangeUser(0.0, 1.2);
+  g2->SetFillColorAlpha(kBlue, 0.3);
+  g2->SetLineColor(kBlue);
+  g2->Draw("3 same");
+
+  TLegend* l = new TLegend(0.7, 0.16, 0.88, 0.3);
+  l->SetFillColor(0);
+  l->SetBorderSize(0);
+  l->AddEntry(g, "NeNe", "f");
+  l->AddEntry(g2, "OO", "f"); 
+  l->Draw(); 
+  c->SaveAs("MinBiasTheory.pdf");
+  
+  // write to a root file
+  TFile* outFile = new TFile("MinBias.root", "RECREATE");
+  outFile->cd();
+  g->SetName("NeNeMinBias");
+  g->Write();
+  g2->SetName("OOMinBias");
+  g2->Write();
+  outFile->Close();
+  
+}
+
+// function to get the graph from json
+TGraphAsymmErrors*  getGraphFromJSON(std::string inputFile, std::string datasetName) {
+    std::ifstream in(inputFile);
     json j;
     in >> j;
 
     // assuming we only care about min bias for the moment. 
-    auto data = j["NeNe_000_100_Pi0"];
+    auto data = j[datasetName];
 
     std::vector<double> x_vals, y_central, y_err;
     std::vector<double> x_err_low, x_err_high; 
@@ -43,18 +83,5 @@ void makeGraphFromJSON() {
     g->SetLineColor(kRed);
     g->SetLineWidth(2);
 
-    // plot just to check if it's correct
-    TCanvas *c = new TCanvas("c","c",800,600);
-    g->Draw("A3");   // "3" = draw filled band
-    g->GetHistogram()->GetXaxis()->SetTitle("#it{p}_{T}");
-    g->GetHistogram()->GetYaxis()->SetTitle("#it{R}_{AA}");
-    g->GetHistogram()->GetYaxis()->SetRangeUser(0.0, 1.2); 
-    c->SaveAs("NeNeMinBias.pdf");
-
-    // write to a root file
-    TFile* outFile = new TFile("NeNeMinBias.root", "RECREATE");
-    outFile->cd();
-    g->SetName("NeNeMinBias");
-    g->Write();
-    outFile->Close(); 
+    return g; 
 }
